02f4c3cd0150f96c5fe20173b23eaebe0d3d5b6b
petres <peter.reschenhofer@gmail.com>
2020-10-18 12:46:50 +0200

    Limit lines log output

--- api/modules/log.py
+++ api/modules/log.py
@@ -8,12 +8,12 @@ files = ['listener', 'init', 'api']
 
 @bp.route('/<string:file>')
 @bp.route('/<string:file>/<string:format>')
-def logfile(file, format = 'plain'):
+def logfile(file, format = 'plain', lineCount = 500):
     if file not in files:
         abort(404)
     with open(os.path.join(base_folder, 'log/{}.log'.format(file))) as f:
         lines = reversed(f.readlines())
         if format == 'html':
-            return render_template('log.html.j2', lines = lines, file = file, files = files)
+            return render_template('log.html.j2', lines = list(lines)[:lineCount], file = file, files = files)
         else:
             return Response(''.join(lines), mimetype='text/plain')
